a475e2e6ab083fb1c0397e07332f013240d21a48
valeria furin <v.furin@web.de>
2020-11-11 22:00:36 +0100

    Built model with validations

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,5 @@
 class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+
+  validates :name, :address, :category, presence: true, exclusion: { in: %w(neptunian) }
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
 class Review < ApplicationRecord
+  belongs_to :restaurant
+
+  validates :content, presence: { strict: true }
+  validates :rating, presence: { strict: true }, numericality: { only_integer: true }
 end
